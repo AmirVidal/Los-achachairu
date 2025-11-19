17a9b1c62d290a6d31fd8da4ca76e00ed08d648c
AmirVidal <amirvidal2004@gmail.com>
2025-11-19 08:24:53 -0400

    commi

--- Proyecto-Web/src/app.js
+++ Proyecto-Web/src/app.js
@@ -3,10 +3,10 @@ const routes = require('./routes');
 
 const app = express();
 
-// Middlewares básicos
+
 app.use(express.json());
 
-// Rutas
+
 app.use('/api', routes);
 
 module.exports = app;
--- Proyecto-Web/src/routes/index.js
+++ Proyecto-Web/src/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Ruta de prueba
 router.get('/hola', (req, res) => {
   res.json({ mensaje: 'Hola desde el backend con Node y Express' });
 });
