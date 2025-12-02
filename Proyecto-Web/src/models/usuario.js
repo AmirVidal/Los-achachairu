db5f928b1dabbbce657a23c8e22b1d695cbfa9e6
AmirVidal <amirvidal2004@gmail.com>
2025-12-01 21:38:54 -0400

    Usuario

---
+++ Proyecto-Web/src/models/usuario.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const UsuarioSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  correo: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  rol: {
+    type: String,
+    enum: ["gestor", "editor", "encuestado"],
+    required: true
+  },
+
+  autorizadoPorGestor: {
+    type: Boolean,
+    default: false
+  },
+
+  activo: {
+    type: Boolean,
+    default: true
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Usuario", UsuarioSchema);
