861ad175db65363dbd44713913a760ae93e40ca8
PanaShusco <joelllusco1@gmail.com>
2025-12-09 20:30:10 -0400

    cambiar version express de 5 a 4

--- Proyecto-Web/src/server.js
+++ Proyecto-Web/src/server.js
@@ -1,13 +1,28 @@
-require('dotenv').config();
-const app = require('./app');
-const connectDB = require('./config/database');
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const http2 = require("http2");
+
+const app = require("./app");                      // ← CORREGIDO
+const connectDB = require("./config/database");   // ← CORREGIDO
 
 connectDB();
 
+const certPath = path.join(__dirname, "../cert"); // ← CORREGIDO (sube un nivel)
+
+const options = {
+  key: fs.readFileSync(path.join(certPath, "key.pem")),
+  cert: fs.readFileSync(path.join(certPath, "cert.pem")),
+  allowHTTP1: true
+};
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+const server = http2.createSecureServer(options, app);
+
+server.listen(PORT, () => {
+  console.log(`🚀 Servidor HTTPS + HTTP/2 funcionando en https://localhost:${PORT}`);
 });
+
 
 
