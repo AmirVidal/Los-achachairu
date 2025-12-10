adf83e0fb8b1d412d324081074037a92ae40e4fb
AmirVidal <amirvidal2004@gmail.com>
2025-12-09 22:18:40 -0400

    http2

--- Proyecto-Web/src/server.js
+++ Proyecto-Web/src/server.js
@@ -1,21 +1,12 @@
 require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
-const https = require("https");
+const express = require("express");
 const app = require("./app");
 const connectDB = require("./config/database");
 
 connectDB();
 
-const certPath = path.join(__dirname, "../cert");
+const PORT = 3000;
 
-const options = {
-  key: fs.readFileSync(path.join(certPath, "key.pem")),
-  cert: fs.readFileSync(path.join(certPath, "cert.pem"))
-};
-
-const PORT = process.env.PORT || 3000;
-
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Servidor HTTPS funcionando en https://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en http://localhost:${PORT}`);
 });
