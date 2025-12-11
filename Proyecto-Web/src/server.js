d88516fd26cb244792edf04c450b178871289c24
PanaShusco <joelllusco1@gmail.com>
2025-12-11 13:11:30 -0400

    aniadido https para express

--- Proyecto-Web/src/server.js
+++ Proyecto-Web/src/server.js
@@ -1,12 +1,27 @@
 require("dotenv").config();
-const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const http = require("http");
+const https = require("https");
 const app = require("./app");
 const connectDB = require("./config/database");
 
 connectDB();
 
-const PORT = 3000;
+const certPath = path.join(__dirname, "../cert");
 
-app.listen(PORT, () => {
-  console.log(`Servidor Express escuchando en http://localhost:${PORT}`);
+const sslOptions = {
+  key: fs.readFileSync(path.join(certPath, "key.pem")),
+  cert: fs.readFileSync(path.join(certPath, "cert.pem")),
+};
+
+const HTTPS_PORT = 3000;
+const HTTP_PORT = 4000;
+
+https.createServer(sslOptions, app).listen(HTTPS_PORT, () => {
+  console.log(`Servidor EXPRESS HTTPS en https://localhost:${HTTPS_PORT}`);
+});
+
+http.createServer(app).listen(HTTP_PORT, () => {
+  console.log(`Servidor EXPRESS HTTP para nginx en http://localhost:${HTTP_PORT}`);
 });
